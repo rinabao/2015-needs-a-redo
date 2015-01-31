0817967b9bd9cb453ab57cc8051c33a068222ddc
Grant Scott <GrantScott@missouri.edu>
2015-01-31 10:00:31 -0600

    added my first sentence, I may do this again for practice in an hour

---
+++ sentences/scottgs.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+
+// Function to include & executeA
+int scottgsSentence1()
+{
+  printf("%s\n","The mean professor, who is a Beaman Monster, made a class redo a homework exercise");
+  return 1;
+}
+
+
+
