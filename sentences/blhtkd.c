f0f823916a3bf25e5a977baef310f8e7dabf326b
Brooke Haile-Mariam <blhtkd@mail.missouri.edu>
2015-01-31 14:44:33 -0600

    Fixing file

--- sentences/blhtkd.c
+++ sentences/blhtkd.c
@@ -3,10 +3,12 @@
 void blhtkd_sentence1()
 {
   printf("Then more and more students, who were really Andean Wolves, began fixing their assignments.\n");
+
 }
 
 void blhtkd_sentence2()
 {
   printf("Everyone joined in and watched the movie.\n");
+
 }
 
