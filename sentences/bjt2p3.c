5e3daaa32a374d5fd8a9c15948094d8fe81b7e17
bjt2p3_112816 <bjt2p3@babbage.cs.missouri.edu>
2015-01-31 15:59:46 -0600

    Wrote two sentences; Ran tests

--- sentences/bjt2p3.c
+++ sentences/bjt2p3.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 void bjt2p3_sentence1(void) {
-	printf("Tmp\n");
+	printf("\nBut still the hero of the story was nowhere to be found.\n");
 }
 
 void bjt2p3_sentence2(void) {
-	printf("Tmp\n");
+	printf("The ninjas walked away. All the students finished Ip Man and started watching Ong Bak 2\n");
 }
