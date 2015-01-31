c4ba7581449eb413ab5c599d35e2548ba26aa368
Thenry945 <travis@henrichs.cc>
2015-01-31 13:02:28 -0600

    added my file

---
+++ sentences/tehww4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int tehww4_s1()
+{
+	printf("The hero of this story is nowhere to be found\n");
+	return 0;
+}
+
+int tehww4_s2()
+{
+	printf("So the class organized a massive search party to find the hero to slay this Beaman Monster posing as the professor\n");
+	return 0;
+}
