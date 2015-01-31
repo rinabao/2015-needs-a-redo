b3cfd283ce7941e15e807e739b8a1929e018d142
Alex Drake <amd2b7@mail.misouri.edu>
2015-01-31 16:59:12 -0600

    dded my sentences and stuff

---
+++ sentences/amd2b7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int amd2b7_1(){
+printf("But they suddenly realized they had forgotten something important!");
+return 1;
+}
+
+int amd2b7_2(){
+printf("They had forgotten their most important task, the dreaded SWE Exercise 2 was due in mere minutes...\n\n");
+return 1;
+}
