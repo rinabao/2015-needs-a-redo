40149761c56f6715f25e540247173d503ed0a8e2
Jayme Becker <jebd4f@mail.missouri.edu>
2015-02-01 15:47:48 -0600

    fixed my error by adding my .c file to the sentences folder

---
+++ sentences/jebd4f.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int jebd4f_Sentence1()
+{
+	printf("The professor then rememberd that the majestic 'Ahool' could defeat the goat suckers as well");
+	return 1; 
+}
+
+
+int jebd4f_Sentence2()
+{
+	printf("After remembering this very important fact, the professor took flight on his Ahool into the darkness to try and find the goat suckers!");
+	return 1;
+}
