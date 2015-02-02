47611f9de676367ed8a48733b61accb4973b02db
indiahampton <iphvg5@mail.missouri.edu>
2015-02-01 18:24:47 -0600

    Had to delete a semi-colon that was not needed

--- sentences/iphvg5.c
+++ sentences/iphvg5.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
-int iphvg5_sentence1();
+int iphvg5_sentence1()
 {
 
 	printf("\n The professor made them redo the assignment because of a craxy Devil Bird threatened him to do so.\n");
 	return 1;
 }
 
-int iphvg5_sentence2();
+int iphvg5_sentence2()
 {
 
 	printf("\n Tony had a girlfriend that was called a Globster.\n");
