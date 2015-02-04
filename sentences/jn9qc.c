af1858825473f0e42a60ac6e54e10a118e64059a
jn9qc <jn9qc@mail.missouri.edu>
2015-02-04 07:49:52 -0600

    added sentences

---
+++ sentences/jn9qc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int jn9qc_s1(){
+	printf("\nTony Supermano and his ragtag gang of Ngoubou were at war with a gang of Owlmen.\n");
+	return 1;
+	}
+
+int jn9qc_s2(){
+	printf("One night, the gang of Owlmen challenged the gang of Ngoubou to a brawl over control of the afterlife.\n");
+	return 1;
+}
+
