fc76fc73a4ecbb848df5f8cc5d1071e26c53185f
djry35 <djry35@mail.missouri.edu>
2015-02-03 19:19:26 -0600

    I'm currently adding my sentences, does it work?

---
+++ sentences/djry35.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void djry35_1()
+{
+	printf("\nLink finished his bigfoot story, and the class of CS 4320 applauded. One person in the crowd happened to be paying attention though, and said, 'What does this have to do with the rest of the story?'");
+}
+
+void djry35_2()
+{
+	printf("\n'Yeah!' Said another student. 'What happened to Tony?'\n");
+}
