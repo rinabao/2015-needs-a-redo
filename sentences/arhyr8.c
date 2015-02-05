afffd1d47015196a0970ccc96f3a541009738f38
Adam Hitchens <arhitchens@gmail.com>
2015-02-05 11:31:45 -0600

    added sentences

---
+++ sentences/arhyr8.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+
+// Function to include & executeA
+int arhyr8Sentence1()
+{
+  printf("But the pirates were not alone.");
+  return 1;
+}
+
+int arhyr8Sentence2()
+{
+  printf("With them, they brought a ferocious pack of jackalopes.");
+  return 1;
+}
+
+
+
