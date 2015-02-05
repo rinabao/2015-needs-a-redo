f3df97acc60c94ba5d05b7c1be635c1b31e1aa6f
Alex Foster <acfr47@mail.missouri.edu>
2015-02-05 16:25:45 -0600

    trying to add my C file

---
+++ sentences/acfr47.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int acfr47_Sentence1(){
+printf("It's a Romantic Comedy about a Chuchunya from Moscow, and his adventures in NYC with a group of otherwise normal software engineers.\n");
+return 1;
+}
+
+int acfr47_Sentence2(){
+printf("This Chuchunya's name was Jeffrey, and he was so consumed by the Beaman monster that he devoted his entire Saturday to finding him.\n");
+return 1;
+}
