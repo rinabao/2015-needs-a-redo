bb996119e78b09cd471c92d28bb20edf397667f9
Andrew Craighead <apcvzb@mail.missouri.edu>
2015-02-05 21:17:43 -0600

    commit

---
+++ sentences/apcvzb.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+
+// Function to include & executeA
+int apcvzbSentence1()
+{
+	printf("\nINSERT SENTENCE ONE\n");
+      	return 1;
+}
+
+int apcvzbSentence2()
+{
+	printf("\nINSERT SENTENCE TWO \n");
+	return 1;
+}
+
